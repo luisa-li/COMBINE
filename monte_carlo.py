5c69d291ad911f383f5deb989e9aadc7ee304963
Luisa <103768169+luisa-li@users.noreply.github.com>
2024-05-25 10:53:15 -0400

    monte carlo

---
+++ monte_carlo.py
@@ -0,0 +1,31 @@
+import random
+
+# area of a circle definition = pi * r^2 
+# area of a the square enclosing it = 4 * r^2 
+# area of circle / area of square = pi / 4
+# dots inside circle / dots inside square = pi / 4
+# (dots inside circle / dots inside square) * 4 = pi 
+
+def estimate_pi(total_points: int) -> float:
+    """Estimates PI via Monte Carlo given the total number of points to use."""
+
+    inside = 0
+    for _ in range(total_points):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x ** 2 + y ** 2 < 1:
+            inside += 1
+    pi = 4 * (inside / total_points)
+    return pi
+
+def main(): 
+    print(estimate_pi(10000))
+    print(estimate_pi(100000))
+    print(estimate_pi(1000000))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
